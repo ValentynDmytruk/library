62153e54e95022b99d0ba9dd54596a3907c3b03e
Dmytruk <dmytruk.v_ak23@nuwm.edu.ua>
2026-06-08 23:39:19 +0300

    Fix

--- library.py
+++ library.py
@@ -1,29 +1,28 @@
-from interfaces import Subject, Observer
+from interfaces import Observer
 from book import Book
 
-class Library(Subject):
+class Library:
     _instance = None
+    _observers = []
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance._observers = []
-            cls._instance.books = []
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Library, cls).__new__(cls, *args, **kwargs)
+            cls._instance.books = []  # Список для збереження книг
         return cls._instance
 
     def attach(self, observer: Observer):
+        """Реєстрація нового спостерігача"""
         if observer not in self._observers:
             self._observers.append(observer)
 
     def detach(self, observer: Observer):
+        """Видалення спостерігача зі списку"""
         if observer in self._observers:
             self._observers.remove(observer)
 
-    def notify(self, book_title: str):
-        for obs in self._observers:
-            obs.update(book_title)
-
-    def register_book(self, book: Book):
+    def add_book(self, book: Book):
+        """Додавання книги та автоматичне сповіщення всіх спостерігачів"""
         self.books.append(book)
-        print(f"\n--- [Система]: Книгу '{book.title}' успішно додано ---")
-        self.notify(book.title)
+        for observer in self._observers:
+            observer.update(book.title)
